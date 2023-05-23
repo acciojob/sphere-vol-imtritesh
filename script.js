92e7aa3e9f922e93e78f9d36f3e26c11faa7da46
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-23 16:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius = document.querySelector("#radius").value;
+	let PI = Math.PI;
+	let volume = (4 * PI * radius * radius * radius)/3;
+	document.querySelector("#volume").value = volume;
+  return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
